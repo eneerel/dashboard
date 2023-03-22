f449c14dff0a19fe7c0950689cc76db27ebe7b90
Evening <evening@mstars-lab3-07s-iMac.local>
2023-03-22 17:51:27 +0800

    first commit

---
+++ src/context/authContext.js
@@ -0,0 +1,9 @@
+import { createContext, useContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
+};
+export default AuthProvider;
